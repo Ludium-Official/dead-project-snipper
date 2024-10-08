d919e4b2671a93ab4454ef59c24fe5caa1d8214a
kihoon <caleb9510@gmail.com>
2024-10-08 21:34:52 +0900

    modified : GithubRepoActivityLog

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,53 +1,75 @@
+-- Table: Projects
 CREATE TABLE Projects (
-  project_id UUID PRIMARY KEY,
-  project_name VARCHAR,
-  created_date TIMESTAMP,
-  recent_action_date TIMESTAMP
+    project_id UUID PRIMARY KEY,
+    project_name VARCHAR,
+    project_url VARCHAR,
+    created_date TIMESTAMP,
+    recent_action_date TIMESTAMP
 );
 
+-- Table: FundedMembers
 CREATE TABLE FundedMembers (
-  project_id UUID REFERENCES Projects(project_id),
-  name VARCHAR
+    project_id UUID REFERENCES Projects(project_id),
+    name VARCHAR
 );
 
+-- Table: XAccounts
 CREATE TABLE XAccounts (
-  project_id UUID REFERENCES Projects(project_id),
-  account_id VARCHAR UNIQUE
+    project_id UUID REFERENCES Projects(project_id),
+    account_id VARCHAR UNIQUE
 );
 
+-- Table: GithubRepos
 CREATE TABLE GithubRepos (
-  project_id UUID REFERENCES Projects(project_id),
-  github_repo_id UUID UNIQUE,
-  owner_name VARCHAR,
-  github_repo VARCHAR
+    project_id UUID REFERENCES Projects(project_id),
+    github_repo_id UUID UNIQUE,
+    owner_name VARCHAR,
+    github_repo VARCHAR
 );
 
+-- Table: NearAddress
 CREATE TABLE NearAddress (
-  project_id UUID REFERENCES Projects(project_id),
-  near_address VARCHAR UNIQUE
+    project_id UUID REFERENCES Projects(project_id),
+    near_address VARCHAR UNIQUE
 );
 
+-- Table: XActivityLog
 CREATE TABLE XActivityLog (
-  account_id VARCHAR REFERENCES XAccounts(account_id),
-  tweet VARCHAR,
-  retweet_count INT,
-  like_count INT,
-  reply_count INT,
-  quote_count INT,
-  follower_count INT,
-  datetime TIMESTAMP
+    account_id VARCHAR REFERENCES XAccounts(account_id),
+    tweet VARCHAR,
+    retweet_count INT,
+    like_count INT,
+    reply_count INT,
+    quote_count INT,
+    follower_count INT,
+    datetime TIMESTAMP
 );
 
+-- Table: GithubRepoActivityLog
 CREATE TABLE GithubRepoActivityLog (
-  github_repo_id UUID REFERENCES GithubRepos(github_repo_id),
-  commit_count INT,
-  issue_count INT,
-  PR_count INT,
-  watcher_count INT,
-  datetime TIMESTAMP
+    github_repo_id UUID REFERENCES GithubRepos(github_repo_id),
+    daily_commit_count INT,
+    daily_open_issue_count INT,
+    daily_closed_issue_count INT,
+    daily_pull_count INT,
+    daily_merge_count INT,
+    watchers_count INT,
+    stars_count INT,
+    forks_count INT,
+    datetime TIMESTAMP
 );
 
+-- Table: OnChainActivityLog
 CREATE TABLE OnChainActivityLog (
-  near_address VARCHAR REFERENCES NearAddress(near_address),
-  Activity_log VARCHAR
+    near_address VARCHAR REFERENCES NearAddress(near_address),
+    activity_log VARCHAR
+);
+
+-- Table: ProjectReport
+CREATE TABLE ProjectReport (
+    project_id UUID REFERENCES Projects(project_id),
+    report VARCHAR,
+    created_date TIMESTAMP,
+    modified_date TIMESTAMP,
+    most_recent BOOLEAN
 );
